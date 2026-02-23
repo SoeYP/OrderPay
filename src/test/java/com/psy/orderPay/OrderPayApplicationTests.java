dd24a233d6d22d890fe2823bd031a53a815ca7b5
박소영 <psyoung@bagsoyeongs-MacBook-Pro.local>
2026-02-23 14:32:27 +0900

    spring boot 설정

---
+++ src/test/java/com/psy/orderPay/OrderPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psy.orderPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
